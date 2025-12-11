b697751bdf9a527698cba4877e7f553019d1b7fa
Muhammad-Aditya-putra <cellutama814@gmail.com>
2025-12-11 23:27:01 +0800

    Create Tipe data array satu dimensi, dua dimensi, tiga dimensi.py

---
+++ FINAL TEST/Tipe data array satu dimensi, dua dimensi, tiga dimensi.py
@@ -0,0 +1,32 @@
+#Nama : Muhammad Aditya Putra
+#NIM : D0425516
+#Prodi : Sistem Informasi
+
+#tipe data array satu dimensi
+
+nama_alamat_umur = ["Muhammad Aditya Putra", "Majene",19]
+print (nama_alamat_umur)
+
+#tipe data array dua dimensi
+
+nama_alamat_umur_dan_buah = [
+    ["Muhammad Aditya Putra", "Majene, 19],
+    ["durian", "mangga", "pepaya"],
+]
+
+#Print all
+print(nama_alamat_umur_dan_makanan)
+
+#tipe data array tiga dimensi
+
+nama_teman_sisfoB = [
+    [
+        ["adit", "masriadi"],
+        ['mahmud', "maswill"]
+    ],
+    [
+        ["ibrahim", "aulia"],
+        ["ihsan", "dermawan"]
+    ]
+]
+print(nama_teman_sisfoB)
